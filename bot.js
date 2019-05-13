0ec32d10a31eb30a42199573d128e185e200a13a
MattheW0512 <50520746+MattheW0512@users.noreply.github.com>
2019-05-13 15:28:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'anyád') {
+    	message.reply('kurva');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
