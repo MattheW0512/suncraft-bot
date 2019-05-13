8d9eac1def4cf745b666a952f74dfa9e9baee3bd
MattheW0512 <50520746+MattheW0512@users.noreply.github.com>
2019-05-13 15:45:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'Cseuz') {
-    	message.reply('kurva');
+    	message.reply('buzi');
   	}
 });
 
