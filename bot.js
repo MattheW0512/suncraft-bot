b2a4c1a82ecc52b887dda49f9334836abae5df94
MattheW0512 <50520746+MattheW0512@users.noreply.github.com>
2020-07-17 22:21:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,11 +12,17 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-if (message.member.roles.find(r => r.name === "teszt")){
-user = message.member        
-const otherServer = client.guilds.get("733759129511919717"); 
-const role =server.roles.find('name', 'teszt2');
-server.user.addRole(role);
+    if (message.content === 'xd') {
+    	message.reply('...');
+  	}
+});
+
+client.on('message', message => {
+    if (message.member.roles.find(r => r.name === "teszt")){
+        user = message.member        
+        const otherServer = client.guilds.get("733759129511919717"); 
+        const role =server.roles.find('name', 'teszt2');
+        server.user.addRole(role);
 }})
 
 // THIS  MUST  BE  THIS  WAY
