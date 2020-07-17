feb17a8fac6361d854b81226c97aeee966f4846a
MattheW0512 <50520746+MattheW0512@users.noreply.github.com>
2020-07-17 22:49:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message', message => {
         user = message.member
         const guild = client.guilds.get("733759129511919717");
         const role =guild.roles.find('name', 'teszt2');
-        guild.user.addRole(role);
+        server.user.addRole(role);
 }})
 
 // THIS  MUST  BE  THIS  WAY
