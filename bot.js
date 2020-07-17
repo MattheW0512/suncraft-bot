e2e068577b6e13bd266690d4ccd77b828e8ae182
MattheW0512 <50520746+MattheW0512@users.noreply.github.com>
2020-07-17 22:17:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,9 +15,9 @@ client.on('message', message => {
 if (message.member.roles.find(r => r.name === "teszt")){
 user = message.member        
 const otherServer = client.guilds.get("733759129511919717"); 
-const role = otherServer.roles.find("name", "teszt2");
+const role =server.roles.find('name', 'teszt2');
 server.user.addRole(role);
-}});
+}})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
